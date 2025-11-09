a7805a0c33109d190a878e2813f0b35955194775
Ahmetcan Arslan <ahmetcanarslan25@gmail.com>
2025-11-09 12:03:07 +0300

    Evaluation of t5-efficient-small-MLSUM-TR-fine-tuned

---
+++ models/t5/t5_evaluate_base_model.py
@@ -0,0 +1,79 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+import pandas as pd
+from tqdm import tqdm
+import evaluate
+import torch
+
+MODEL_ID = "Turkish-NLP/t5-efficient-small-MLSUM-TR-fine-tuned"
+DATA_CSV_PATH = "../../data/processed/cleaned_dataset.csv"
+TEXT_COL = "cleaned_article"     
+SUMMARY_COL = "cleaned_summary"   
+OUTPUT_FILE = "t5_evaluate_output.csv"
+
+if torch.backends.mps.is_available():
+    device = "mps"
+    print("Using device: Apple M4/MPS (GPU)") # Test for apple silicon cpu
+elif torch.cuda.is_available():
+    device = 0
+    print("Using device: NVIDIA CUDA (GPU)")
+else:
+    device = "cpu"
+    print("Using device: CPU")
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_ID)
+
+summarizer = pipeline("summarization", model=model, tokenizer=tokenizer, device=device)
+
+df = pd.read_csv(DATA_CSV_PATH).fillna("")
+df = df.head(100)
+print(f"✅ Loaded {len(df)} samples from CSV")
+
+results = []
+for i, row in tqdm(df.iterrows(), total=len(df)):
+    text = row[TEXT_COL]
+    ref = row[SUMMARY_COL]
+
+    if not text:
+        results.append({"original": text, "reference": ref, "summary": ""})
+        continue
+
+    try:
+        input_text = f"summarize: {text}" 
+
+        summary = summarizer(
+            input_text,
+            max_length=150,
+            min_length=40,
+            do_sample=False,
+            truncation=True
+        )[0]["summary_text"]
+
+    except Exception as e:
+        print(f"⚠️ Error on row {i}: {e}")
+        summary = ""
+
+    results.append({
+        "original": text,
+        "reference": ref,
+        "summary": summary
+    })
+
+out_df = pd.DataFrame(results)
+out_df.to_csv(OUTPUT_FILE, index=False)
+print(f"Results are saved to {OUTPUT_FILE}")
+
+rouge = evaluate.load("rouge")
+preds = out_df["summary"].tolist()
+refs = out_df["reference"].tolist()
+
+metrics = rouge.compute(predictions=preds, references=refs)
+print("\n📊 ROUGE results (F1 scores):")
+for k, v in metrics.items():
+    print(f"{k}: {v:.3f}")
+
+# ROUGE results (F1 scores):
+# rouge1: 0.589
+# rouge2: 0.555
+# rougeL: 0.576
+# rougeLsum: 0.575
